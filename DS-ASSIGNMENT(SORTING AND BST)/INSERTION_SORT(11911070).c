4675e36c756a299de58398208b956eb03997dce3
Mr. Garv Singhal <codergarv22@gmail.com>
2020-06-01 16:25:23 +0530

    Add files via upload

---
+++ DS-ASSIGNMENT(SORTING AND BST)/INSERTION_SORT(11911070).c
@@ -0,0 +1,39 @@
+//PROGRAMME MADE BY GARV SINGHAL
+
+//ROLL NO-11911070 BRANCH - CSE
+
+//PROGRAMME FOR INSERTION SORT AND IS WRITTEN IN C LANGUAGE
+
+ #include<stdio.h>
+ #include<stdlib.h>
+ void main()
+ {
+   int n;
+   printf("ENTER THE SIZE OF ARRAY:-");
+   scanf("%d",&n);
+   int *ptr;
+   ptr=(int *)malloc(n*sizeof(int));
+   printf("ENTER LIST NOW:-");
+   for(int a=0;a<n;a++)
+   {
+       scanf("%d",&ptr[a]);
+   }
+   for(int b=1;b<n;b++)
+   {
+	   int x=ptr[b];
+	   for(int c=b-1;c>=0;c--)
+	   {
+		   if(x<ptr[c])
+		   {
+			   ptr[c+1]=ptr[c];
+			   ptr[c]=x;
+		   }
+		   else
+		    break;
+	   }
+   }
+   for(int d=0;d<n;d++)
+   {
+	   printf("%d  ",ptr[d]);
+   }
+ }
